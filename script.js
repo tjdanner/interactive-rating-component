9c00855c1fde37b1807eadc6cb1d28f9ad6fbda9
Trevor Danner <tjdanner01@gmail.com>
2023-01-25 14:21:35 -0600

    Completed.

--- script.js
+++ script.js
@@ -2,16 +2,24 @@ const submit = document.getElementById('submit');
 const buttons = document.getElementsByClassName('button');
 const purple = getComputedStyle(document.querySelector(':root')).getPropertyValue('--purple');
 const lightGrey = getComputedStyle(document.querySelector(':root')).getPropertyValue('--light-grey');
+const score = document.getElementById('score');
+const ratingSelect = document.getElementById('rating-select');
+const submittedBox = document.getElementById('sumbitted');
 
-submit.addEventListener('mouseover', event => {
+mouseoverSubmit = () => {
     submit.style.backgroundColor = 'white';
     submit.style.color = purple;
-})
+    submit.style.fontSize = '';
+}
 
-submit.addEventListener('mouseout', event => {
+submit.addEventListener('mouseover', mouseoverSubmit)
+
+mouseoutSubmit = () => {
     submit.style.backgroundColor = purple;
     submit.style.color = 'white';
-})
+}
+
+submit.addEventListener('mouseout', mouseoutSubmit);
 
 buttonActive = () => {
     for (let i = 0; i < buttons.length; i++) {
@@ -47,7 +55,56 @@ for (let i = 0; i < buttons.length; i++) {
         buttons[i].style.color = 'white';
         event.target.removeEventListener('mouseout', buttonRevert)
         event.target.removeEventListener('mouseover', buttonActive)
+
+        submit.innerHTML = 'submit';
+        submit.style.backgroundColor = '';
+        submit.style.color = '';
+        submit.style.fontSize = '';
+        submit.addEventListener('mouseover', mouseoverSubmit);
+        submit.addEventListener('mouseout', mouseoutSubmit);
     })
 }
 
+submit.addEventListener('click', submitEvent => {
+    if (buttons[0].style.color === 'white') {
+        score.innerHTML = `You selected ${buttons[0].innerHTML} out of 5`
+        ratingSelect.style.display = 'none';
+        submitted.style.display = 'flex';
+    } else if (buttons[1].style.color === 'white') {
+        score.innerHTML = `You selected ${buttons[1].innerHTML} out of 5`
+        ratingSelect.style.display = 'none';
+        submitted.style.display = 'flex';
+    } else if (buttons[2].style.color === 'white') {
+        score.innerHTML = `You selected ${buttons[2].innerHTML} out of 5`
+        ratingSelect.style.display = 'none';
+        submitted.style.display = 'flex';
+    } else if (buttons[3].style.color === 'white') {
+        score.innerHTML = `You selected ${buttons[3].innerHTML} out of 5`
+        ratingSelect.style.display = 'none';
+        submitted.style.display = 'flex';
+    } else if (buttons[4].style.color === 'white') {
+        score.innerHTML = `You selected ${buttons[4].innerHTML} out of 5`
+        ratingSelect.style.display = 'none';
+        submitted.style.display = 'flex';
+    } else {
+        submit.innerHTML = "Please select a rating."
+        submit.style.backgroundColor = 'red';
+        submit.style.color = 'white';
+        submit.removeEventListener('mouseout', mouseoutSubmit)
+        submit.removeEventListener('mouseover', mouseoverSubmit)
+        submit.addEventListener('mouseout', event => {
+            submit.style.backgroundColor = 'red';
+            submit.style.color = 'white';
+        })
+        submit.addEventListener('mouseover', event => {
+            submit.style.backgroundColor = 'white';
+            submit.style.color = 'red';
+        })
+    }
 
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].style.color = ''
+        buttons[i].style.backgroundColor = ''
+    }
+    
+})
